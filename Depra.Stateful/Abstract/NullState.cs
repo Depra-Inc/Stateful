ad6e26a19ca317a6a5ca16a7d7ec7c92a6809e13
godzzz <g0dzZz1lla@yandex.ru>
2023-08-07 01:25:50 +0400

    NullState; Solution renamed

---
+++ Depra.Stateful/Abstract/NullState.cs
@@ -0,0 +1,21 @@
+﻿// Copyright © 2022-2023 Nikolay Melnikov. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+
+namespace Depra.Stateful.Abstract
+{
+	public sealed class NullState : IState
+	{
+		void IState.Enter() => throw new NullStateException();
+
+		void IState.Exit() => throw new NullStateException();
+
+		private sealed class NullStateException : Exception
+		{
+			private const string MESSAGE = "Null state is not allowed.";
+
+			public NullStateException() : base(MESSAGE) { }
+		}
+	}
+}
