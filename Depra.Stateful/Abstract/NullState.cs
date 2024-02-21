25674b2fdbf19996d395cb080ef67ffcc6931fd2
godzzz <g0dzZz1lla@yandex.ru>
2024-02-22 03:47:09 +0400

    Cleaning

--- Depra.Stateful/Abstract/NullState.cs
+++ Depra.Stateful/Abstract/NullState.cs
@@ -7,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace Depra.Stateful.Abstract
 {
-	public sealed class NullState : IState
+	public readonly struct NullState : IState
 	{
 		void IState.Enter() => throw new NullStateException();
 		void IState.Exit() => throw new NullStateException();
+
 		Task IState.EnterAsync(CancellationToken cancellationToken) => throw new NullStateException();
 		Task IState.ExitAsync(CancellationToken cancellationToken) => throw new NullStateException();
 
