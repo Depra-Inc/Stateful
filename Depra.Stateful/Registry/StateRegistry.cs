aeebb52ac86ef5824226505b2873684801fb65ab
n.melnikov <n.melnikov@depra.org>
2025-08-23 00:31:15 +0400

    Add state registry & extensions

---
+++ Depra.Stateful/Registry/StateRegistry.cs
@@ -0,0 +1,42 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2022-2025 Depra <n.melnikov@depra.org>
+
+using System;
+using System.Collections.Generic;
+using Depra.Stateful.Abstract;
+
+namespace Depra.Stateful.Registry
+{
+	public sealed class StateRegistry
+	{
+		private readonly Dictionary<Type, object> _states = new();
+
+		public bool IsRegistered(Type type) => _states.ContainsKey(type);
+
+		public IState Get(Type type) => _states.TryGetValue(type, out var state)
+			? (IState)state
+			: throw new KeyNotFoundException($"State of type {type} is not registered.");
+
+		public bool TryGet(Type type, out IState state)
+		{
+			if (_states.TryGetValue(type, out var obj))
+			{
+				state = (IState)obj;
+				return true;
+			}
+
+			state = null;
+			return false;
+		}
+
+		public void Register(IState state) => Register(state.GetType(), state);
+
+		public void Register(Type type, IState state)
+		{
+			if (!_states.TryAdd(type, state))
+			{
+				throw new InvalidOperationException($"State of type {type} is already registered.");
+			}
+		}
+	}
+}
---
+++ Depra.Stateful/Registry/StateRegistryExtensions.cs
@@ -0,0 +1,36 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2022-2025 Depra <n.melnikov@depra.org>
+
+using System.Runtime.CompilerServices;
+using Depra.Stateful.Abstract;
+
+namespace Depra.Stateful.Registry
+{
+	public static class StateRegistryExtensions
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool IsRegistered<TState>(this StateRegistry self) where TState : IState =>
+			self.IsRegistered(typeof(TState));
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static TState Get<TState>(this StateRegistry self) where TState : IState =>
+			(TState)self.Get(typeof(TState));
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool TryGet<TState>(this StateRegistry self, out TState state) where TState : IState
+		{
+			if (self.TryGet(typeof(TState), out var obj))
+			{
+				state = (TState)obj;
+				return true;
+			}
+
+			state = default;
+			return false;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Register<TState>(this StateRegistry self, TState state) where TState : IState =>
+			self.Register(typeof(TState), state);
+	}
+}
